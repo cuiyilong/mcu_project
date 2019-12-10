d78d4f7529a2dedf3a0c242d4076258d96724149
cuiyilong <cuiyilong@gmail.com>
2019-12-10 23:21:24 +0800

    1210_update

--- temperature_control/HARDWARE/MAX6675/max_6675.c
+++ temperature_control/HARDWARE/MAX6675/max_6675.c
@@ -19,8 +19,8 @@
 
 
 #define 	MAX6675_CS	 		GPIO_Pin_4
-#define 	MAX6675_CSL()		GPIOA->BRR = MAX6675_CS;
-#define 	MAX6675_CSH()		GPIOA->BSRR = MAX6675_CS;
+#define 	MAX6675_CSL()		GPIOA->BSRRL = MAX6675_CS;
+#define 	MAX6675_CSH()		GPIOA->BSRRH = MAX6675_CS;
 
 /*
  * 函数名：SPI1_Init
@@ -31,55 +31,55 @@
  */																						  
 void SPI_MAX6675_Init(void)
 {
-	GPIO_InitTypeDef GPIO_InitStructure;
-	SPI_InitTypeDef  SPI_InitStructure;	
-	
-	/* 使能 SPI1 时钟 */       
+    GPIO_InitTypeDef GPIO_InitStructure;
+    SPI_InitTypeDef  SPI_InitStructure;	
+
+    /* 使能 SPI1 时钟 */       
 
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);//使能GPIOA时钟
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);//使能SPI1时钟
-  
-	/* ---------通信I/O初始化----------------
-	 * PA5-SPI1-SCK :MAX6675_SCK
-	 * PA6-SPI1-MISO:MAX6675_SO
-	 * PA7-SPI1-MOSI:MAX6675_SI	 
-	 */
+
+    /* ---------通信I/O初始化----------------
+    * PA5-SPI1-SCK :MAX6675_SCK
+    * PA6-SPI1-MISO:MAX6675_SO
+    * PA7-SPI1-MOSI:MAX6675_SI	 
+    */
 
 
-      GPIO_PinAFConfig(GPIOA,GPIO_PinSource5,GPIO_AF_SPI1); //GPIOA5复用为SPI1-CLK
-	GPIO_PinAFConfig(GPIOA,GPIO_PinSource6,GPIO_AF_SPI1); //GPIOA6复用为SPI1-MISO
-	GPIO_PinAFConfig(GPIOA,GPIO_PinSource7,GPIO_AF_SPI1); //GPIOA7复用为SPI1-MOSI
+    GPIO_PinAFConfig(GPIOA,GPIO_PinSource5,GPIO_AF_SPI1); //GPIOA5复用为SPI1-CLK
+    GPIO_PinAFConfig(GPIOA,GPIO_PinSource6,GPIO_AF_SPI1); //GPIOA6复用为SPI1-MISO
+    GPIO_PinAFConfig(GPIOA,GPIO_PinSource7,GPIO_AF_SPI1); //GPIOA7复用为SPI1-MOSI
 
-    
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;			// 复用输出
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-	/* ---------控制I/O初始化----------------*/
-	/* PA4-SPI1-NSS:MAX6675_CS */							// 片选
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;		// 推免输出
-	GPIO_Init(GPIOA, &GPIO_InitStructure);						  
-	GPIO_SetBits(GPIOA, GPIO_Pin_4);						// 先把片选拉高，真正用的时候再拉低
- 
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;			// 复用输出
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-	/* SPI1 配置 */ 
-	SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
-	SPI_InitStructure.SPI_Mode = SPI_Mode_Master;
-	SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;
-	SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low;
-	SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge;
-	SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;
-	SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_8;
-	SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
-	SPI_InitStructure.SPI_CRCPolynomial = 7;
-	SPI_Init(SPI1, &SPI_InitStructure);
-	
-	
-	/* 使能 SPI1  */
-	SPI_Cmd(SPI1, ENABLE); 
+    /* ---------控制I/O初始化----------------*/
+    /* PA4-SPI1-NSS:MAX6675_CS */							// 片选
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;		// 推免输出
+    GPIO_Init(GPIOA, &GPIO_InitStructure);						  
+    GPIO_SetBits(GPIOA, GPIO_Pin_4);						// 先把片选拉高，真正用的时候再拉低
+
+
+    /* SPI1 配置 */ 
+    SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
+    SPI_InitStructure.SPI_Mode = SPI_Mode_Master;
+    SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;
+    SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low;
+    SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge;
+    SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;
+    SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_8;
+    SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
+    SPI_InitStructure.SPI_CRCPolynomial = 7;
+    SPI_Init(SPI1, &SPI_InitStructure);
+
+
+    /* 使能 SPI1  */
+    SPI_Cmd(SPI1, ENABLE); 
 }
 
 /*
@@ -90,17 +90,17 @@ void SPI_MAX6675_Init(void)
 unsigned char MAX6675_ReadByte(void)
 {
 	
-	/* Loop while DR register in not emplty */
-	while(SPI_I2S_GetFlagStatus(SPI1,SPI_I2S_FLAG_TXE) == RESET);
-	
-	/* Send byte through the SPI1 peripheral */
-	SPI_I2S_SendData(SPI1, 0xff);
-	
-	/* Wait to receive a byte */
-	while(SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_RXNE) == RESET);
-	
-	/* Return the byte read from the SPI bus */
-	return SPI_I2S_ReceiveData(SPI1);
+    /* Loop while DR register in not emplty */
+    while(SPI_I2S_GetFlagStatus(SPI1,SPI_I2S_FLAG_TXE) == RESET);
+
+    /* Send byte through the SPI1 peripheral */
+    SPI_I2S_SendData(SPI1, 0xff);
+
+    /* Wait to receive a byte */
+    while(SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_RXNE) == RESET);
+
+    /* Return the byte read from the SPI bus */
+    return SPI_I2S_ReceiveData(SPI1);
 }
 /*
  * 函数名：main
@@ -120,9 +120,10 @@ unsigned char MAX6675_ReadByte(void)
 	/* MAX6675 SPI 接口初始化 */
 	SPI_MAX6675_Init();
 
+    printf("\r\n max_6675_main!!!\r\n");
+
 	while(1)
 	{
-	
 		MAX6675_CSL();
 		c = MAX6675_ReadByte();
 		i = c;
@@ -151,8 +152,9 @@ unsigned char MAX6675_ReadByte(void)
 		{
 			printf("max6675没有数据返回，请检查max6675连接。\r\n");
 		}
-		for(i=0;i<0x2fffff;i++);			//max6675的转换时间是0.2秒左右，所以两次转换间隔不要太近
-	}
+		//for(i=0;i<0x2fffff;i++);			//max6675的转换时间是0.2秒左右，所以两次转换间隔不要太近
+            delay_ms(200);
+    }
 }
 
 
